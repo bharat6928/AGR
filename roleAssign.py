5340f976e520a1e1701192738d1cf93789fc839c
bharat6928 <bharatpurohit692@gmail.com>
2020-01-07 21:44:32 +0530

    USER ROLE MAPPING

--- roleAssign.py
+++ roleAssign.py
@@ -8,67 +8,80 @@ class roleAssign:
         return connection
 
     def roleMaping(self,c):
-        
 
+        #database name: USER_MANAGER
+        #SAP table name: AGR_USERS with 2 attribute UNAME(user),AGR_NAME(role))
+        
+        #Create new Users if not present in SAP
         cursor = c.cursor()
-        selectSap = "SELECT uname,agr_name FROM user_manager.agr_users as l LEFT JOIN mysql.default_roles as r on(l.uname=r.user) WHERE uname not in(SELECT user FROM mysql.default_roles)"
-        cursor.execute(selectSap)
-        rowsC = cursor.fetchall()
+        select_sap = "SELECT uname,agr_name FROM user_manager.agr_users as l LEFT JOIN mysql.default_roles as r on(l.uname=r.user) WHERE uname not in(SELECT user FROM mysql.default_roles)"
+        cursor.execute(select_sap)
+        rows_insert = cursor.fetchall()
         c.commit()
         rowcount = cursor.rowcount
         if rowcount > 0:
-            for row in rowsC:
+            for row in rows_insert:
                 row1 = row[0]
                 row2 = row[1]
                 createUser = "CREATE USER '"+ row1 +"'@'localhost'"
                 cursor.execute(createUser)
                 c.commit()
-                grantRole = "GRANT '"+ row2 +"' to '"+ row1 +"'@'localhost'"
+                grantRole = "GRANT '"+ row2 +"' TO '"+ row1 +"'@'localhost'"
                 cursor.execute(grantRole)
                 c.commit()
 
                 setDefaultRole = "SET DEFAULT ROLE '"+ row2 +"' TO '"+ row1 +"'@'localhost' "
                 cursor.execute(setDefaultRole)
                 c.commit()
+            print("\n\n"+str(rowcount)+" New User Added successfully\n")
 
-        selectUpdate = "SELECT uname,agr_name FROM user_manager.agr_users as l LEFT JOIN mysql.default_roles as r on(l.uname=r.user) WHERE l.uname = r.user and l.agr_name != r.default_role_user"
+
+        #Upadate role of user if role changed in SAP
+        selectUpdate = "SELECT uname,agr_name FROM user_manager.agr_users AS l LEFT JOIN mysql.default_roles AS r ON(l.uname=r.user) WHERE l.uname = r.user AND l.agr_name != r.default_role_user"
         cursor.execute(selectUpdate)
-        rowsU = cursor.fetchall()
+        rows_update = cursor.fetchall()
         c.commit()
         rowcount2 = cursor.rowcount
         
         if rowcount2 > 0:
-            for row in rowsU:
+            for row in rows_update:
                 row1 = row[0]
                 row2 = row[1]
-                select_current_role = "SELECT default_role_user from mysql.default_roles where user = '"+ row1 +"' "
+                select_current_role = "SELECT default_role_user FROM mysql.default_roles WHERE user = '"+ row1 +"' "
                 cursor.execute(select_current_role)
                 c.commit()
                 current_role = cursor.fetchone()
-                revokeR = "REVOKE '"+ current_role[0] +"' FROM '"+ row1 +"'@'localhost'"
-                cursor.execute(revokeR)
+                revoke_old_role = "REVOKE '"+ current_role[0] +"' FROM '"+ row1 +"'@'localhost'"
+                cursor.execute(revoke_old_role)
                 c.commit()
-                grantUR = "GRANT '"+ row2 +"' TO '"+ row1 +"'@'localhost'"
-                cursor.execute(grantUR)
+                grant_new_role = "GRANT '"+ row2 +"' TO '"+ row1 +"'@'localhost'"
+                cursor.execute(grant_new_role)
                 c.commit()
-                setUDefaultRole = "SET DEFAULT ROLE '"+ row2 +"' TO '"+ row1 +"'@'localhost'"
-                cursor.execute(setUDefaultRole)
+                set_new_DefaultRole = "SET DEFAULT ROLE '"+ row2 +"' TO '"+ row1 +"'@'localhost'"
+                cursor.execute(set_new_DefaultRole)
                 c.commit()
-        selectD = "select user,default_role_user from mysql.default_roles where user not in(select uname from user_manager.agr_users)"
+            print(str(rowcount2)+" New User Updated successfully\n")
+
+            
+        #delete user and assigned role if user is not present in SAP
+        selectD = "SELECT user,default_role_user FROM mysql.default_roles WHERE user NOT IN(SELECT uname FROM user_manager.agr_users)"
         cursor.execute(selectD)
-        rowsD = cursor.fetchall()
+        rows_delete = cursor.fetchall()
         c.commit()
         rowcount3 = cursor.rowcount
         if rowcount3 > 0:
-            for row in rowsD:
+            for row in rows_delete:
                 row1 = row[0]
                 row2 = row[1]
-                revokeD = "REVOKE '"+ row2 +"' From '"+ row1 +"'@'localhost'"
-                cursor.execute(revokeD)
+                revoke_role = "REVOKE '"+ row2 +"' From '"+ row1 +"'@'localhost'"
+                cursor.execute(revoke_role)
                 c.commit()
-                dropU = "DROP user '"+ row1 +"'@'localhost'"
-                cursor.execute(dropU)
+                drop_user = "DROP user '"+ row1 +"'@'localhost'"
+                cursor.execute(drop_user)
                 c.commit()
+            print(str(rowcount3)+" User Deleted successfully\n")
+
+        print("\nUSER Role mapping successfull\n\n")
 
 
 
